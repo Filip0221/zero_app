ecf936a9d993ae426772fda109c3841c020f8f96
Filip Skup <filipskup231@gmail.com>
2024-02-08 16:22:45 +0100

    Initial Commit

---
+++ zero_app/zero_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  zero_appApp.swift
+//  zero_app
+//
+//  Created by Filip Skup on 08/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct zero_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
